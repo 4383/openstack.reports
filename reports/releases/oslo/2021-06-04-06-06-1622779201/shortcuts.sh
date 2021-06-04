cb2d37ed9777ecb98a6cf25622b3cc2040d47038
Hervé Beraud <hberaud@redhat.com>
2021-06-04 06:33:04 +0200

    [report] Oslo releases report ven. juin  4 06:33:03 CEST 2021

---
+++ reports/releases/oslo/2021-06-04-06-06-1622779201/shortcuts.sh
@@ -0,0 +1,13 @@
+cd_oslo
+cd releases
+if [ -d openstack.reports ]; then
+    rm -rf openstack.reports
+fi
+git clone git@github.com:4383/openstack.reports
+cd_releses
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-06-04-06-06-1622779201/master.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-06-04-06-06-1622779201/wallaby.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-06-04-06-06-1622779201/victoria.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-06-04-06-06-1622779201/ussuri.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-06-04-06-06-1622779201/train.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-06-04-06-06-1622779201/independent.sh
