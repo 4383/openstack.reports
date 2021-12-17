3963a1f9b45b428a07e3af069ad66e763220f10e
Hervé Beraud <hberaud@redhat.com>
2021-12-17 06:33:39 +0100

    [report] Oslo releases report ven. déc. 17 06:33:39 CET 2021

---
+++ reports/releases/oslo/2021-12-17-06-12-1639717201/shortcuts.sh
@@ -0,0 +1,13 @@
+cd_oslo
+cd releases
+if [ -d openstack.reports ]; then
+    rm -rf openstack.reports
+fi
+git clone git@github.com:4383/openstack.reports
+cd_releses
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-12-17-06-12-1639717201/master.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-12-17-06-12-1639717201/xena.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-12-17-06-12-1639717201/wallaby.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-12-17-06-12-1639717201/victoria.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-12-17-06-12-1639717201/ussuri.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2021-12-17-06-12-1639717201/independent.sh
