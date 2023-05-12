6a8a3ab838dbe2b69fdc201f9ef4a3c44744e9fd
Hervé Beraud <hberaud@redhat.com>
2023-05-12 06:21:07 +0200

    [report] Oslo releases report ven. 12 mai 2023 06:21:07 CEST

---
+++ reports/releases/oslo/2023-05-12-06-05-1683864001/shortcuts.sh
@@ -0,0 +1,13 @@
+cd_oslo
+cd releases
+if [ -d openstack.reports ]; then
+    rm -rf openstack.reports
+fi
+git clone git@github.com:4383/openstack.reports
+cd_releses
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2023-05-12-06-05-1683864001/master.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2023-05-12-06-05-1683864001/zed.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2023-05-12-06-05-1683864001/yoga.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2023-05-12-06-05-1683864001/xena.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2023-05-12-06-05-1683864001/wallaby.sh
+sh ~/dev/redhat/upstream/openstack/oslo/releases/openstack.reports/reports/releases/oslo/2023-05-12-06-05-1683864001/independent.sh
